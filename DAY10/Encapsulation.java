fb5843a87a0418082220b2bd7a7418051501deb6
kaushi-kka <kaushijai@gmail.com>
2026-03-31 09:10:38 +0530

    DAY10

--- DAY10/Encapsulation.java
+++ DAY10/Encapsulation.java
@@ -2,15 +2,49 @@ package DAY10;
 class Bankaccount{
     private double balance;
     public void setBalance(double balance){
-        this.balance=balance;
+        if(balance>0)
+        {
+            this.balance=balance;
+        }
+        else{
+            System.out.println("Invalid balance amount");
+        }
     }
-}
+    public void withdraw(int amount){
+        if(amount>0 && amount<=balance){
+            balance-=amount;
+            System.out.println("Amount withdrawn: " + amount);
+        }
+        else{
+            System.out.println("Invalid withdrawal amount");
+        }
+    }
+    public void deposit(int amount)
+    {
+        if(amount>0)
+            {
+                balance+=amount;
+                System.out.println("Amount credited: " + amount);
+            }
+            else{
+                System.out.println("Invalid amount credited");
+            }
+        }
+    public double getBalance(){
+        return balance;
+}}
+
 public class Encapsulation 
 {
     public static void main(String[] args) 
     {
+
     Bankaccount b=new Bankaccount();
-    b.balance+=-1000;
-    System.out.print(b.balance);
+    b.setBalance(1000);
+    System.out.println(b.getBalance());
+    b.withdraw(600);
+    System.out.println(b.getBalance());
+    b.deposit(10000);
+    System.out.println(b.getBalance());
     }
 }
