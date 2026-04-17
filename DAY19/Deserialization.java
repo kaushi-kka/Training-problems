38c66af03ea1008f4e12e8729e448457ec507ef9
kaushi-kka <kaushijai@gmail.com>
2026-04-17 09:12:34 +0530

    DAY19

--- DAY19/Deserialization.java
+++ DAY19/Deserialization.java
@@ -1,5 +1,23 @@
 package DAY19;
-
-public class Deserialization {
-    
+import java.io.*;
+public class DeSerialization {
+    public static void main(String[] args) throws Exception {
+        FileInputStream fi= new FileInputStream("Student.txt");
+        ObjectInputStream ObjStream=new ObjectInputStream(fi);
+        //Student s=(Student)ObjStream.readObject();
+        while(true)
+        {
+            try
+            {
+                Student s=(Student) ObjStream.readObject();
+                System.out.println(s.name+" "+s.mark);
+            }
+            catch(EOFException e){
+                //e.printStackTrace();
+                break;
+            }
+        }
+        fi.close();
+        ObjStream.close();
+    }
 }
